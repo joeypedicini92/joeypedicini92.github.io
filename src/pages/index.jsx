a1040b767413fcd06d865144da93c7331aa2f3b9
Joey Pedicini <joey.pedicini@gmail.com>
2023-12-01 10:15:20 -0500

    weee

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from 'next/future/image'
 import Head from 'next/head'
 import Link from 'next/link'
 import clsx from 'clsx'
 
 import { Button } from '@/components/Button'
-import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
 import {
-  SoundcloudIcon,
-  InstagramIcon,
   GitHubIcon,
   LinkedInIcon,
 } from '@/components/SocialIcons'
@@ -17,11 +15,6 @@ import image2 from '@/images/photos/image-2.jpg'
 import image3 from '@/images/photos/image-3.jpg'
 import image4 from '@/images/photos/image-4.jpg'
 import image5 from '@/images/photos/image-5.jpg'
-import logoAmazon from '@/images/logos/amazon.svg'
-import logoBattleface from '@/images/logos/battleface.svg'
-import logoCOTA from '@/images/logos/cota.svg'
-import logoChase from '@/images/logos/chase.svg'
-import { formatDate } from '@/lib/formatDate'
 import { Testimonial } from '@/components/Testimonial'
 
 function MailIcon(props) {
@@ -42,42 +35,6 @@ function MailIcon(props) {
       <path
         d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
         className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
-
-function BriefcaseIcon(props) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
-
-function ArrowDownIcon(props) {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
       />
     </svg>
   )
@@ -117,88 +74,6 @@ function Newsletter() {
         </Button>
       </div>
     </form>
-  )
-}
-
-function Resume() {
-  let resume = [
-    {
-      company: 'Amazon',
-      title: 'Frontend Engineer II',
-      logo: logoAmazon,
-      start: 'April 2022',
-      end: {
-        label: 'Present',
-        dateTime: new Date().getFullYear(),
-      },
-    },
-    {
-      company: 'battleface',
-      title: 'Senior Software Engineer',
-      logo: logoBattleface,
-      start: 'September 2021',
-      end: 'April 2022',
-    },
-    {
-      company: 'COTA',
-      title: 'Senior Software Engineer',
-      logo: logoCOTA,
-      start: 'October 2016',
-      end: 'September 2021',
-    },
-    {
-      company: 'JP Morgan Chase',
-      title: 'Application Developer',
-      logo: logoChase,
-      start: 'January 2013',
-      end: 'October 2016',
-    },
-  ]
-
-  return (
-    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
-      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
-        <BriefcaseIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Work</span>
-      </h2>
-      <ol className="mt-6 space-y-4">
-        {resume.map((role, roleIndex) => (
-          <li key={roleIndex} className="flex gap-4">
-            <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image src={role.logo} alt="" className="h-7 w-7" unoptimized />
-            </div>
-            <dl className="flex flex-auto flex-wrap gap-x-2">
-              <dt className="sr-only">Company</dt>
-              <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
-                {role.company}
-              </dd>
-              <dt className="sr-only">Role</dt>
-              <dd className="text-xs text-zinc-500 dark:text-zinc-400">
-                {role.title}
-              </dd>
-              <dt className="sr-only">Date</dt>
-              <dd
-                className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
-                aria-label={`${role.start.label ?? role.start} until ${role.end.label ?? role.end
-                  }`}
-              >
-                <time dateTime={role.start.dateTime ?? role.start}>
-                  {role.start.label ?? role.start}
-                </time>{' '}
-                <span aria-hidden="true">—</span>{' '}
-                <time dateTime={role.end.dateTime ?? role.end}>
-                  {role.end.label ?? role.end}
-                </time>
-              </dd>
-            </dl>
-          </li>
-        ))}
-      </ol>
-      <Button href="/Joey Pedicini Resume.pdf" target="_blank" variant="secondary" className="group mt-6 w-full">
-        Download Resume
-        <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-      </Button>
-    </div>
   )
 }
 
@@ -266,15 +141,30 @@ export default function Home() {
         </div>
       </Container>
       <Photos />
-      <Testimonial
-        className="mb-24"
-        client={{ name: 'Tejas, GoEB1', logo: null }}
-      >
-        I reached out to Joey for a basic prototype for my immigrant thought leadership platform. Joey was extremely responsive, and provided me a mock prototype pro-bono, without any payments, just so that I could be unblocked.
-        His wireframe diagrams were clean, extremely slick, and helped me move extremely fast. I am a great believer in working with good human beings, and Joey is one of the few I would definitely advocate for.
-        He is ethical, honest, helpful, and empathetic, and it is a pleasure to work with him and his team.
-        I strongly recommend leveraging Joey & his team for any software architecture and implementation.
-      </Testimonial>
+      <Container className="mt-9">
+        <h2 className="text-3xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-4xl">
+          Testimonials
+        </h2>
+        <Testimonial
+          className={"mb-24 mt-6"}
+          client={{ name: 'Nic, The Breakfast Menu', logo: null }}
+        >
+          Joey made my very first foray into development a total breeze. I came to him with little more than an idea, but Joey was able to elicit the right details from me to get us moving down the runway immediately. Faster than I ever expected, he whipped up a proof of concept that inspired confidence and allowed me to iterate and improve the project in ways that I otherwise wouldn't have discovered.
+          <br></br><br></br>
+          From the get-go, Joey was an asset. He took the project on as his own and expertly guided me through the noise and confusion of building our MVP. The process felt clean and highly cost-effective due to Joey's experience, discernment, and skillset in software development as well as plain-language communication.
+          <br></br><br></br>
+          Congrats! You've found your guy.
+        </Testimonial>
+        <Testimonial
+          className={"mb-24 mt-6"}
+          client={{ name: 'Tejas, GoEB1', logo: null }}
+        >
+          I reached out to Joey for a basic prototype for my immigrant thought leadership platform. Joey was extremely responsive, and provided me a mock prototype pro-bono, without any payments, just so that I could be unblocked.
+          His wireframe diagrams were clean, extremely slick, and helped me move extremely fast. I am a great believer in working with good human beings, and Joey is one of the few I would definitely advocate for.
+          He is ethical, honest, helpful, and empathetic, and it is a pleasure to work with him and his team.
+          I strongly recommend leveraging Joey & his team for any software architecture and implementation.
+        </Testimonial>
+      </Container>
     </>
   )
 }
